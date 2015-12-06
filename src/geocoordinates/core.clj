350d3559e612caa5681d4f76d366e2c4c2f3ef82
dilico <dilico@yahoo.it>
2015-12-06 12:29:42 +0000

    specify projection to use as keyword

--- src/geocoordinates/core.clj
+++ src/geocoordinates/core.clj
@@ -1,12 +1,12 @@
 (ns geocoordinates.core
   (:require [geocoordinates.math :as math]))
 
-(def airy-1830-ellipsoid-constants
+(def ^:private airy-1830-ellipsoid-constants
   "Airy 1830 biaxial ellipsoid shape and size (in metres)."
   {:semi-major-axis-a 6377563.396
    :semi-minor-axis-b 6356256.909})
 
-(def national-grid-transverse-mercator-projection-constants
+(def ^:private national-grid-transverse-mercator-projection-constants
   "National Grid Transverse Mercator projection constants
   (latitude and longitude in decimal degrees, easting and northing in metres)."
   {:scale-factor-on-central-meridian-f0 0.9996012717
@@ -15,10 +15,14 @@
    :true-origin-easting-e0 400000
    :true-origin-northing-n0 -100000})
 
-(def national-grid-constants
+(def ^:private national-grid-constants
   "Ellipsoid and projection constants for the National Grid projection."
   {:ellipsoid-constants airy-1830-ellipsoid-constants
    :transverse-mercator-projection-constants national-grid-transverse-mercator-projection-constants})
+
+(def ^:private projections-constants
+  "Map with ellipsoid and projections constants for various projections."
+  {:national-grid national-grid-constants})
 
 (defn- meridional-arc
   "Compute the meridional arc."
@@ -65,66 +69,62 @@
         e2 (/ (- (math/exp af0 2) (math/exp bf0 2)) (math/exp af0 2))
         n (/ (- af0 bf0) (+ af0 bf0))
         et (- easting (:true-origin-easting-e0 (:transverse-mercator-projection-constants constants)))
-        φd (initial-latitude northing
-                             (:true-origin-northing-n0 (:transverse-mercator-projection-constants constants)) 
-                             af0
-                             (math/decimal-degrees->radians (:true-origin-latitude-φ0
-                                                             (:transverse-mercator-projection-constants
-                                                              constants)))
+        φ (initial-latitude northing
+                            (:true-origin-northing-n0 (:transverse-mercator-projection-constants constants)) 
+                            af0
+                            (math/decimal-degrees->radians (:true-origin-latitude-φ0
+                                                            (:transverse-mercator-projection-constants
+                                                             constants)))
                              n
                              bf0)
         ν (/ af0 
-             (math/sqrt (- 1 (* e2 (math/exp (math/sin φd) 2)))))
+             (math/sqrt (- 1 (* e2 (math/exp (math/sin φ) 2)))))
         ρ (/ (* ν (- 1 e2))
-             (- 1 (* e2 (math/exp (math/sin φd) 2))))
+             (- 1 (* e2 (math/exp (math/sin φ) 2))))
         η2 (- (/ ν ρ) 1)]
     
-    {:af0 af0 :bf0 bf0 :e2 e2 :n n :et et :φd φd :ν ν :ρ ρ :η2 η2}))
+    {:af0 af0 :bf0 bf0 :e2 e2 :n n :et et :φ φ :ν ν :ρ ρ :η2 η2}))
 
 (defn- easting-northing->latitude
   "Un-project Transverse Mercator easting and northing back to latitude."
   [easting northing constants]
-  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n et :et φd :φd ν :ν ρ :ρ η2 :η2}
+  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n et :et φ :φ ν :ν ρ :ρ η2 :η2}
         (compute-easting-northing-conversion-parameters easting northing constants)
-        VII (/ (math/tan φd) (* 2 ρ ν))
-        VIII (* (/ (math/tan φd) (* 24 ρ (math/exp ν 3)))
-                (+ 5 (* 3 (math/exp (math/tan φd) 2)) (- η2 (* 9 η2 (math/exp (math/tan φd) 2)))))
-        IX (* (/ (math/tan φd) (* 720 ρ (math/exp ν 5)))
-              (+ 61 (* 90 (math/exp (math/tan φd) 2)) (* 45 (math/exp (math/tan φd) 4))))]
+        VII (/ (math/tan φ) (* 2 ρ ν))
+        VIII (* (/ (math/tan φ) (* 24 ρ (math/exp ν 3)))
+                (+ 5 (* 3 (math/exp (math/tan φ) 2)) (- η2 (* 9 η2 (math/exp (math/tan φ) 2)))))
+        IX (* (/ (math/tan φ) (* 720 ρ (math/exp ν 5)))
+              (+ 61 (* 90 (math/exp (math/tan φ) 2)) (* 45 (math/exp (math/tan φ) 4))))]
     
     (* (/ 180 math/pi)
-       (+ (- φd (* (math/exp et 2) VII)) (- (* (math/exp et 4) VIII) (* (math/exp et 6) IX))))))
+       (+ (- φ (* (math/exp et 2) VII)) (- (* (math/exp et 4) VIII) (* (math/exp et 6) IX))))))
 
 (defn- easting-northing->longitude
   "Un-project Transverse Mercator easting and northing back to longitude."
   [easting northing constants]
-  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n et :et φd :φd ν :ν ρ :ρ η2 :η2}
+  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n et :et φ :φ ν :ν ρ :ρ η2 :η2}
         (compute-easting-northing-conversion-parameters easting northing constants)
         λ0 (math/decimal-degrees->radians (:true-origin-longitude-λ0
                                            (:transverse-mercator-projection-constants
                                             constants)))
-        X (/ (math/exp (math/cos φd) -1) ν)
-        XI (* (/ (math/exp (math/cos φd) -1) (* 6 (math/exp ν 3)))
-              (+ (/ ν ρ) (* 2 (math/exp (math/tan φd) 2))))
-        XII (* (/ (math/exp (math/cos φd) -1) (* 120 (math/exp ν 5)))
-               (+ 5 (* 28 (math/exp (math/tan φd) 2)) (* 24 (math/exp (math/tan φd) 4))))
-        XIIA (* (/ (math/exp (math/cos φd) -1) (* 5040 (math/exp ν 7)))
+        X (/ (math/exp (math/cos φ) -1) ν)
+        XI (* (/ (math/exp (math/cos φ) -1) (* 6 (math/exp ν 3)))
+              (+ (/ ν ρ) (* 2 (math/exp (math/tan φ) 2))))
+        XII (* (/ (math/exp (math/cos φ) -1) (* 120 (math/exp ν 5)))
+               (+ 5 (* 28 (math/exp (math/tan φ) 2)) (* 24 (math/exp (math/tan φ) 4))))
+        XIIA (* (/ (math/exp (math/cos φ) -1) (* 5040 (math/exp ν 7)))
                 (+ 61 
-                   (* 662 (math/exp (math/tan φd) 2))
-                   (* 1320 (math/exp (math/tan φd) 4))
-                   (* 720 (math/exp (math/tan φd) 6))))]
+                   (* 662 (math/exp (math/tan φ) 2))
+                   (* 1320 (math/exp (math/tan φ) 4))
+                   (* 720 (math/exp (math/tan φ) 6))))]
     (* (/ 180 math/pi)
        (- (+ (- (+ λ0 (* et X)) (* (math/exp et 3) XI)) (* (math/exp et 5) XII)) (* (math/exp et 7) XIIA)))))
 
 (defn easting-northing->latitude-longitude
   "Convert easting and northing to latitude and longitude (in decimal degrees)."
-  ([{easting :easting northing :northing}]
-   (easting-northing->latitude-longitude {:easting easting
-                                          :northing northing}
-                                         national-grid-constants))
-  ([{easting :easting northing :northing} constants]
-   {:latitude (easting-northing->latitude easting northing constants)
-    :longitude (easting-northing->longitude easting northing constants)}))
+  [{easting :easting northing :northing} projection]
+  {:latitude (easting-northing->latitude easting northing (projection projections-constants))
+   :longitude (easting-northing->longitude easting northing (projection projections-constants))})
 
 (defn- compute-latitude-longitude-conversion-parameters
   "Compute the parameters used in the conversion from latitude and longitude."
@@ -135,34 +135,34 @@
                (:scale-factor-on-central-meridian-f0 (:transverse-mercator-projection-constants constants)))
         e2 (/ (- (math/exp af0 2) (math/exp bf0 2)) (math/exp af0 2))
         n (/ (- af0 bf0) (+ af0 bf0))
-        φd (math/decimal-degrees->radians latitude)
+        φ (math/decimal-degrees->radians latitude)
         ν (/ af0 
-             (math/sqrt (- 1 (* e2 (math/exp (math/sin φd) 2)))))
+             (math/sqrt (- 1 (* e2 (math/exp (math/sin φ) 2)))))
         ρ (/ (* ν (- 1 e2))
-             (- 1 (* e2 (math/exp (math/sin φd) 2))))
+             (- 1 (* e2 (math/exp (math/sin φ) 2))))
         η2 (- (/ ν ρ) 1)
         p (- (math/decimal-degrees->radians longitude)
              (math/decimal-degrees->radians (:true-origin-longitude-λ0
                                              (:transverse-mercator-projection-constants
                                               constants))))]
     
-    {:af0 af0 :bf0 bf0 :e2 e2 :n n :φd φd :ν ν :ρ ρ :η2 η2 :p p}))
+    {:af0 af0 :bf0 bf0 :e2 e2 :n n :φ φ :ν ν :ρ ρ :η2 η2 :p p}))
 
 (defn- latitude-longitude->easting
   "Project latitude and longitude to Transverse Mercator easting."
   [latitude longitude constants]
-  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n φd :φd ν :ν ρ :ρ η2 :η2 p :p}
+  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n φ :φ ν :ν ρ :ρ η2 :η2 p :p}
         (compute-latitude-longitude-conversion-parameters latitude longitude constants)
-        IV (* ν (math/cos φd))
+        IV (* ν (math/cos φ))
         V (* (/ ν 6)
-             (math/exp (math/cos φd) 3)
-             (- (/ ν ρ) (math/exp (math/tan φd) 2)))
+             (math/exp (math/cos φ) 3)
+             (- (/ ν ρ) (math/exp (math/tan φ) 2)))
         VI (* (/ ν 120) 
-              (math/exp (math/cos φd) 5)
-              (- (+ (- 5 (* 18 (math/exp (math/tan φd) 2)))
-                    (math/exp (math/tan φd) 4)
+              (math/exp (math/cos φ) 5)
+              (- (+ (- 5 (* 18 (math/exp (math/tan φ) 2)))
+                    (math/exp (math/tan φ) 4)
                     (* 14 η2))
-                 (* 58 (math/exp (math/tan φd) 2) η2)))]
+                 (* 58 (math/exp (math/tan φ) 2) η2)))]
     (+ (:true-origin-easting-e0 (:transverse-mercator-projection-constants constants))
        (* p IV)
        (* (math/exp p 3) V)
@@ -171,28 +171,24 @@
 (defn- latitude-longitude->northing
   "Project latitude and longitude to Transverse Mercator northing."
   [latitude longitude constants]
-  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n φd :φd ν :ν ρ :ρ η2 :η2 p :p}
+  (let [{af0 :af0 bf0 :bf0 e2 :e2 n :n φ :φ ν :ν ρ :ρ η2 :η2 p :p}
         (compute-latitude-longitude-conversion-parameters latitude longitude constants)
         φ0 (math/decimal-degrees->radians (:true-origin-latitude-φ0
                                            (:transverse-mercator-projection-constants
                                             constants)))
-        arc (meridional-arc bf0 n φ0 φd)
+        arc (meridional-arc bf0 n φ0 φ)
         I (+ arc (:true-origin-northing-n0
                   (:transverse-mercator-projection-constants
                    constants)))
-        II (* (/ ν 2) (math/sin φd) (math/cos φd))
-        III (* (/ ν 24) (math/sin φd) (math/exp (math/cos φd) 3) 
-               (+ (- 5 (math/exp (math/tan φd) 2)) (* 9 η2)))
-        IIIA (* (/ ν 720) (math/sin φd) (math/exp (math/cos φd) 5) 
-                (+ (- 61 (* 58 (math/exp (math/tan φd) 2))) (math/exp (math/tan φd) 4)))]
+        II (* (/ ν 2) (math/sin φ) (math/cos φ))
+        III (* (/ ν 24) (math/sin φ) (math/exp (math/cos φ) 3) 
+               (+ (- 5 (math/exp (math/tan φ) 2)) (* 9 η2)))
+        IIIA (* (/ ν 720) (math/sin φ) (math/exp (math/cos φ) 5) 
+                (+ (- 61 (* 58 (math/exp (math/tan φ) 2))) (math/exp (math/tan φ) 4)))]
     (+ I (* (math/exp p 2) II) (* (math/exp p 4) III) (* (math/exp p 6) IIIA))))
 
 (defn latitude-longitude->easting-northing
   "Convert latitude and longitude (in decimal degrees) to easting and northing."
-  ([{latitude :latitude longitude :longitude}]
-   (latitude-longitude->easting-northing {:latitude latitude
-                                          :longitude longitude}
-                                         national-grid-constants))
-  ([{latitude :latitude longitude :longitude} constants]
-   {:easting (latitude-longitude->easting latitude longitude constants)
-    :northing (latitude-longitude->northing latitude longitude constants)}))
+  [{latitude :latitude longitude :longitude} projection]
+  {:easting (latitude-longitude->easting latitude longitude (projection projections-constants))
+   :northing (latitude-longitude->northing latitude longitude (projection projections-constants))})
--- test/geocoordinates/core_test.clj
+++ test/geocoordinates/core_test.clj
@@ -2,40 +2,22 @@
   (:require [clojure.test :refer :all]
             [geocoordinates.core :as geo]))
 
-(deftest test-default-easting-northing->latitude-longitude
-  (testing "convert easting and northing to latitude and longitude using default National Grid constants"
-    (are [easting northing latitude longitude] 
-        (= (geo/easting-northing->latitude-longitude {:easting easting
-                                                      :northing northing}) {:latitude latitude
-                                                                            :longitude longitude})
-      651409.903 313177.27 52.65757030193327 1.7179215806451056
-      363606.5 172914.3 51.45355057024397 -2.52382540824567)))
-
 (deftest test-easting-northing->latitude-longitude
   (testing "convert easting and northing to latitude and longitude using National Grid constants"
     (are [easting northing latitude longitude] 
         (= (geo/easting-northing->latitude-longitude {:easting easting
                                                       :northing northing}
-                                                     geo/national-grid-constants) {:latitude latitude
-                                                                                   :longitude longitude})
+                                                     :national-grid) {:latitude latitude
+                                                                      :longitude longitude})
       651409.903 313177.27 52.65757030193327 1.7179215806451056
       363606.5 172914.3 51.45355057024397 -2.52382540824567)))
-
-(deftest test-default-latitude-longitude->easting-northing
-  (testing "convert latitude and longitude to easting and northing using default National Grid constants"
-    (are [easting northing latitude longitude] 
-        (= (geo/latitude-longitude->easting-northing {:latitude latitude
-                                                      :longitude longitude}) {:easting easting
-                                                                              :northing northing})
-      651409.902802227 313177.2699187288 52.65757030193327 1.7179215806451056
-      363606.5000000153 172914.2999999999 51.45355057024397 -2.52382540824567)))
 
 (deftest test-latitude-longitude->easting-northing
   (testing "convert latitude and longitude to easting and northing using National Grid constants"
     (are [easting northing latitude longitude] 
         (= (geo/latitude-longitude->easting-northing {:latitude latitude
                                                       :longitude longitude}
-                                                     geo/national-grid-constants) {:easting easting
-                                                                                   :northing northing})
+                                                     :national-grid) {:easting easting
+                                                                      :northing northing})
       651409.902802227 313177.2699187288 52.65757030193327 1.7179215806451056
       363606.5000000153 172914.2999999999 51.45355057024397 -2.52382540824567)))
