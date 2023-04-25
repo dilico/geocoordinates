2e6ee0cf32976e08bb8c8d0fa3738fdbbc775eca
Mark Wardle <mark@wardle.org>
2023-04-25 10:37:50 +0100

    Bump to v0.2.x
    This generates a version based on number of commits prefixed by 0.2

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'io.github.dilico/geocoordinates)
-(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def version (format "0.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
